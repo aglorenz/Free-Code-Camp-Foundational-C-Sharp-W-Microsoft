231e7c27f0d523e0dc221596423c203cdb9b5f0a
Andrew Lorenz <aglorenz777@gmail.com>
2023-09-10 13:32:50 -0700

    Student Grade assignment

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -1,1 +1,9 @@
-﻿
+﻿string[] fraudulentOrderIDs = {"B123","C234","A345","C15","B177","G3033","C234","B179"};
+
+foreach (string id in fraudulentOrderIDs) 
+{
+    if (id.StartsWith('B'))
+    {
+        Console.WriteLine(id);
+    }
+}
