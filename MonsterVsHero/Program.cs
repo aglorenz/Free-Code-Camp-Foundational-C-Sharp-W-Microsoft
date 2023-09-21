fc771819d9b97dcf68f381ba6db50b6d2b3a9499
Andrew Lorenz <aglorenz777@gmail.com>
2023-09-21 11:44:58 -0700

    Monster VS Hero

---
+++ MonsterVsHero/Program.cs
@@ -0,0 +1,19 @@
+﻿int heroHealth = 10;
+int monsterHealth = 10;
+Random dice = new Random();
+
+while (heroHealth > 0 && monsterHealth > 0)
+{
+    int attack = dice.Next(1, 11);
+    monsterHealth -= attack;
+    Console.WriteLine($"Monster was damaged and lost {attack} health;  Health is now {monsterHealth}");
+
+    if (monsterHealth > 0)
+    {
+        attack = dice.Next(1, 11);
+        heroHealth -= attack;
+        Console.WriteLine($"Hero was damaged and lost {attack} health;  Health is now {heroHealth}");
+    }
+}
+
+Console.WriteLine($"The winner is {(monsterHealth > heroHealth ? "Monster" : "Hero")}");
