014619f1214afc202738d85c3c8ac05e0ae6fcf2
Andrew Lorenz <aglorenz777@gmail.com>
2023-09-20 18:13:06 -0700

    another module completed

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -1,16 +1,58 @@
-﻿string permission = "Manadger";
-int level = 19;
-string message = "";
+﻿// SKU = Stock Keeping Unit. 
+// SKU value format: <product #>-<2-letter color code>-<size code>
+using System.Security.AccessControl;
 
-if (permission.Contains("Admin")) 
+string sku = "01-MN-L";
+
+string[] product = sku.Split('-');
+
+string type = "";
+string color = "";
+string size = "";
+
+switch (product[0])
 {
-   message =  level > 55 ? "Welcome, Super Admin user." : "Welcome, Admin user.";
+    case "01":
+        type = "Sweat shirt";
+        break;
+    case "02":
+        type = "T-Shirt";
+        break;
+    case "03":
+        type = "Sweat pants";
+        break;
+    default:
+        type = "Other";
+        break;
 }
-else if (permission.Contains("Manager"))
+
+switch (product[1])
 {
-    message = level >= 20 ? "Contact an Admin for access." : "You do not have sufficient privileges.";
+    case "BL":
+        color = "Black";
+        break;
+    case "MN":
+        color = "Maroon";
+        break;
+    default:
+        color = "White";
+        break;
 }
-else message = "You do not have sufficient privileges.";
 
-Console.WriteLine(message);
+switch (product[2])
+{
+    case "S":
+        size = "Small";
+        break;
+    case "M":
+        size = "Medium";
+        break;
+    case "L":
+        size = "Large";
+        break;
+    default:
+        size = "One Size Fits All";
+        break;
+}
 
+Console.WriteLine($"Product: {size} {color} {type}");
