fb5c30fe177f66371702176a96ee2bb9221ff3ea
Andrew Lorenz <aglorenz777@gmail.com>
2023-10-14 14:51:25 -0700

    Part 4 module 2 unit 3 completed

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -1,19 +1,30 @@
-﻿int value1 = 12;
-decimal value2 = 6.2m;
-float value3 = 4.3f;
+﻿string[] pallets = { "B14", "A11", "B12", "A13" };
+Console.WriteLine("");
 
-// Your code here to set result1
-// Hint: You need to round the result to nearest integer (don't just truncate)
-int result1 = Convert.ToInt32(value1 / value2);
-Console.WriteLine($"Divide value1 by value2, display the result as an int: {result1}");
+Array.Clear(pallets, 0, 2);
+Console.WriteLine($"Clearing 2 ... count: {pallets.Length}");
+foreach (var pallet in pallets)
+{
+    Console.WriteLine($"-- {pallet}");
+}
 
-// Your code here to set result2
-decimal result2 = (value2 / (decimal)value3);
-Console.WriteLine($"Divide value2 by value3, display the result as a decimal: {result2}");
+Console.WriteLine("");
+Array.Resize(ref pallets, 6);
+Console.WriteLine($"Resizing 6 ... count: {pallets.Length}");
 
-// Your code here to set result3
-float result3 = value3 / value1;
-Console.WriteLine($"Divide value3 by value1, display the result as a float: {result3}");
+pallets[4] = "C01";
+pallets[5] = "C02";
 
-decimal result4 = 0; 
-bool success = decimal.TryParse("1.2345", out result4);
+foreach (var pallet in pallets)
+{
+    Console.WriteLine($"-- {pallet}");
+}
+
+Console.WriteLine("");
+Array.Resize(ref pallets, 3);
+Console.WriteLine($"Resizing 3 ... count: {pallets.Length}");
+
+foreach (var pallet in pallets)
+{
+    Console.WriteLine($"-- {pallet}");
+}
